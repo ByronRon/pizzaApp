c1ea37d1275beeb9d55c7de0de8e9732bf4d5b72
Byron Ron H <amendicovobis29@gmail.com>
2022-03-15 22:27:17 -0500

    chore: get pizza list

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Featured from "../components/Featured";
 import PizzaList from "../components/PizzaList";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ pizzaList }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured />
-      <PizzaList />
+      <PizzaList pizzaList={pizzaList} />
     </div>
   );
 }
